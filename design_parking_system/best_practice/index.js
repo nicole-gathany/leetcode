14e7d5df992646550097e9a0946e6996cad31a5c
nicole-gathany <nicole.gathany@gmail.com>
2021-05-16 19:34:44 -0400

    added instructions and comments on my understanding of the best practice solution

--- design_parking_system/best_practice/index.js
+++ design_parking_system/best_practice/index.js
@@ -1,6 +1,11 @@
 var ParkingSystem = function(big, medium, small) {
     this.count = [big, medium, small];
+    //this.count[0] === big;
+    //this.count[1] === medium;
+    //this.count[2] ===small;
 };
+
+//Parkingsystem(1, 1, 0) would looke like this this.count = [1, 1, 0]
 
 /** 
  * @param {number} carType
@@ -8,6 +13,13 @@ var ParkingSystem = function(big, medium, small) {
  */
 ParkingSystem.prototype.addCar = function(carType) {
   return this.count[carType - 1]-- > 0;
+  //addCar(1)
+  // carType is 1 (big) that's asking if there is a big space. 
+  //if big (or this.count[0] or this.count[1-0]) is 0, return false
+  //if it's more than 0 return false and then subtract 1 from this.big
+  //addCar(2)
+  //if carTyppe
+  
 };
 
 /** 
